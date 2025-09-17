da88a803be224fc92f8660e11e9595d05cbc87db
Mbonimana <mbonimana12@gmail.com>
2025-09-17 22:44:34 +0200

    Adding Cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/databaseConfiguration";
 import productRouter from "./routes/productPath";
 import userRouter from "./routes/userPath";
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+app.use(express.json());
+app.use(cors());
 app.use(express.json());
 
 
