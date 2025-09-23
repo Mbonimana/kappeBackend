6651361bb48fc641ce28d9acb56b0f5d846f35b5
Mbonimana <mbonimana12@gmail.com>
2025-09-23 07:43:30 +0200

    Add Product delete api

--- src/Routes/productPath.ts
+++ src/Routes/productPath.ts
@@ -1,10 +1,11 @@
-import { createProduct ,getProducts  } from "../controllers/productController";
+import { createProduct ,getProducts,deleteProduct  } from "../controllers/productController";
 
 import express from "express";
 
 const productRouter = express();
 productRouter.post('/create-product', createProduct);
 productRouter.get('/get-products', getProducts);
+productRouter.delete('/delete-product/:id', deleteProduct);
 
 
 
