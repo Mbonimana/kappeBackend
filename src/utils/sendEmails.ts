82f84ae78abbac15a224f6d8118fc930a55c6cd6
Mbonimana <mbonimana12@gmail.com>
2025-10-03 13:04:27 +0200

    Corrected

--- src/utils/sendEmails.ts
+++ src/utils/sendEmails.ts
@@ -1,45 +1,37 @@
-  import nodemailer from 'nodemailer';
-  import {Transporter, SendMailOptions } from 'nodemailer';
-  import dotenv from 'dotenv';
-  dotenv.config();
+import nodemailer from "nodemailer";
+import { Transporter } from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
+const transporter: Transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_NAME,
+    pass: process.env.EMAIL_PASSWORD, // 16-char app password
+  },
+});
 
-    const transporter: Transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_NAME,
-      pass: process.env.EMAIL_PASSWORD, // your 16-char app password
-    },
-  });
-  //function to send mail
+const mailsender = async (
+  to: string,
+  subject: string,
+  htmlContent: string
+): Promise<boolean> => {
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_NAME,
+      to,
+      subject,
+      html: htmlContent,
+    });
+    console.log("Email Successfully Sent");
+    return true;
+  } catch (error: any) {
+    console.error("Sending Mail Error", error);
+    return false;
+  }
+};
 
-  const mailsender = async (
-    to: string,
-    subject: string,
-    htmlContent: string,
-    ): Promise<boolean>=>{
-      try {
-        const SendMailOptions = {
-          from: process.env.EMAIL_NAME,
-          to,
-          subject,
-          html: htmlContent,
-        };
-
-        await transporter.sendMail(SendMailOptions);
-        console.log("Email Successfully");
-        return true;
-      } catch(error:any){
-        console.error("Sending Mail Error", error);
-        return false;
-      }
-    
-    };
-
-    
-  export default mailsender;
-    
-  
-
+export default mailsender;
